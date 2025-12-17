89a8e07adb7f3b3622bb3476c14c195d972da38b
bhargaviakula15-prog <bhargaviakula15@gmail.com>
2025-12-17 06:18:04 +0000

    container config done

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,3 +1,8 @@
+
+// ********************** conversions ******************************
+
+
+
 /*
 let score = 33 // number type
 
@@ -8,14 +13,14 @@ console.log(typeof(score));
 
 let score = "33" // string 
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
 //if we want to work on numbers and our variable is in diff format then we will convert them
 
 let valueInNumber = Number(score) //converting score into number
-console.log(typeof valueInNumber) //checking type
-console.log(valueInNumber) //printing the variable 
+//console.log(typeof valueInNumber) //checking type
+//console.log(valueInNumber) //printing the variable 
 
 //"33" => 33
 //"33abc" => NaN (not a number)
@@ -25,7 +30,7 @@ console.log(valueInNumber) //printing the variable
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 
 //1 ==> true, 0 => false
@@ -37,5 +42,52 @@ console.log(booleanIsLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof(stringNumber))
+//console.log(stringNumber)
+//console.log(typeof(stringNumber))
+
+
+
+// ********************** operations ******************************
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2%2);
+// console.log(2/2);
+
+let str1 = "hi"
+let str2 = " hello" // if u did not give space before h all the contetnt is merging with out space in output
+let str3 = str1 + str2
+//console.log(str3);
+
+console.log("1" +2);
+console.log(1 +"2");
+console.log("1" +2 + 2);// output: 122 //if we have string at beginning it is merging all the values
+console.log(1 + 2 +"2"); //output: 32 //if we have string at end it is adding starting values and then merging the values
+
+//console.log(true) // output: true
+//console.log(+true) // 1
+//console.log(true+) // error
+//console.log(+"") // 0
+
+// study postfix and prefix operators 
+//https://tc39.es/ecma262/#sec-type-conversion
+
+
+
+let x = 3;
+const y = x++; // 1st X is value is goint to y, so we get initial value to y , later the value is getting increment and added to x
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3" 
+
+let a = 3;
+const b = ++a; // 1st a is value is getting increment and assigning to b and later same incremeted value is assigning to a
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"  

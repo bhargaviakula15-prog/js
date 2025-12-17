69d09d60a5697b936e4f5afe4c983072b8e45a3d
bhargaviakula15-prog <bhargaviakula15@gmail.com>
2025-12-17 07:27:05 +0000

    container config done

---
+++ 01_basic/datatypes_Summary.js
@@ -0,0 +1,27 @@
+// primitive
+
+// 7 types : string, number, boolean, null, undefined, symbol(make it unique), bigint
+
+const id = Symbol('123')
+const anotherId = Symbol('123') // both are different
+
+const bigNumber = 64638936372928262527293n
+
+
+
+// regerance (non primitive)
+// array, objects, functions
+
+// array
+const heros = ["batman", "ironman"] 
+
+//objects {}
+let myObj = {
+    name: "bhargavi",
+    age: 22
+}
+
+//function(){} 
+const myFunction = function(){
+    console.log("hello world")
+}

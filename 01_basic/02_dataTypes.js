d0b897e6b3bcafbd74fe7714c0f12184d441ca38
bhargaviakula15-prog <bhargaviakula15@gmail.com>
2025-12-17 05:07:03 +0000

    container config done

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict;" // treat all js code as newer version
+
+//alert( 3 + 3) // we are using nodejs, not browser
+
+let name = "bh" //string
+let age = 18 // numeric
+let isLoggedIn = true // boolean
+
+//number => 2 to power 53
+//bigint
+// string => ""
+//boolean => true/false
+// null => standalone value (empty)
+// undefined => (value not assigned) // ex: let state;
+//symbol => unique
+
+
+//object
+
+
+// null is of type object
+//undefined is of type undefined
+
+console.log(typeof"bh");
+console.log(typeof 1);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+

df320352d6a7abf0884c12f67f12d93a159b3c5e
bhargaviakula15-prog <bhargaviakula15@gmail.com>
2025-12-22 05:58:02 +0000

    container config done

---
+++ 01_basic/07_dates.js
@@ -0,0 +1,27 @@
+let myDate = new Date()
+// console.log(myDate); //2025-12-22T05:28:32.299Z
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toDateString());
+
+
+//let createMyDate = new Date( 2025, 1, 13)
+//let createMyDate = new Date( 2025, 1, 13, 5, 3)
+//let createMyDate = new Date("2025-01-13")
+let createMyDate = new Date("01-24-2025")// mm-dd-yyyy
+//console.log(createMyDate.toLocaleString());
+
+let timeStamp = Date.now()
+// console.log(timeStamp) // milliseconds
+// console.log(Math.floor(Date.now()/1000)); //converted to seconds
+
+// get get date or month -- get()
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth());
+// console.log(newDate.getDate());
+
